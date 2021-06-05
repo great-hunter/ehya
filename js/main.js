7ac72706cacd69c2b012ac147a0b8d11fd34b463
great-hunter <greathunter@mail.ru>
2021-06-05 23:31:47 +0300

    create: 'Главный эран десктоп'

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  
+  //Мобильное меню
+  var menuButton = $(".navbar-mobile__button");
+  menuButton.on('click', function () {
+    $(".navbar__menu").toggleClass('navbar__menu--mobile--visible');
+  });
+
+
+
+
+
+
+
+
+
+});
